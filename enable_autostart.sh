1930469b16a6ee3b642efb4ce9ca27768c2d7087
Brendon Taylor <team@lanslide.com.au>
2026-07-06 12:55:02 +1000

    Added extra files

--- enable_autostart.sh
+++ enable_autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sed -i 's/x-restart-policy: \&restart-policy "no"/x-restart-policy: \&restart-policy "unless-stopped"/g' docker-compose.yml
-docker-compose up -d
+docker compose up -d
